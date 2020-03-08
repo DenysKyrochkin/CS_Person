fd9c402520e3d4e31293d051cb414753d62e7068
Denys <denys@MacBook-Pro-Denys.local>
2020-03-08 12:04:27 +0200

    release v1

---
+++ Practice01/Models/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice02.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public static DateTime Today = DateTime.Today;
+
+        public Person(string name, string surname, string email, DateTime birthday)
+        {
+            Name = name; Surname = surname; Email = email; Birthday = birthday;
+        }
+
+        public Person(string name, string surname, string email)
+        {
+            Name = name; Surname = surname; Email = email;
+        }
+
+        public Person(string name, string surname, DateTime birthday)
+        {
+            Name = name; Surname = surname; Birthday = birthday;
+        }
+
+        public static int GetAge(DateTime birthDate)
+        {
+            return Today.Year - birthDate.Year - 1 +
+                ((Today.Month > birthDate.Month || Today.Month == birthDate.Month && Today.Day >= birthDate.Day) ? 1 : 0);
+        }
+
+        public bool IsAdult
+        {
+            get
+            {
+                if(GetAge(Birthday) >= 18)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public string SunSign => Zodiac.ZodiacWestern(Birthday.Day, Birthday.Month);
+
+        public string ChineseSign => Zodiac.ZodiacChinese(Birthday.Year);
+
+        public bool IsBirthday
+        {
+
+            get
+            {
+                
+                if (Today.Month == Birthday.Month && Today.Day == Birthday.Day)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Practice01/Pages/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Practice02.Models;
+
+namespace Practice02.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string Header { get; set; }
+        public string Information { get; set; }
+
+        public void OnGet()
+        {
+            Header = "Input information about yourself!";
+
+        }
+        public void OnPost(string name, string surname, string email, DateTime date)
+        {
+            var years = Person.GetAge(date);
+
+            if (years < 0)
+            {
+                Header = "Hello, man from the future!";
+                Information = $"Please, input correct date. (before {Person.Today.ToShortDateString()})";
+            }
+
+            else if (years > 135)
+            {
+                Header = "Hello, man from the afterlife!";
+                Information = $"Please, input correct date. (after {Person.Today.Year - 135} year). You are {years}";
+            }
+            else
+            {
+                Person person = new Person(name, surname, email, date);
+                Header = "Thank you for your information. ";
+                if (person.IsAdult)
+                {
+                    Header += "And Happy Birthday to you!";
+                }
+
+                Information = $"Now we know that you are {person.Name} {person.Surname}. Your email is {person.Email}. Your birthday date is {person.Birthday.ToShortDateString()}. We know that you are ";
+                if (person.IsAdult) { Information += "an adult. "; }
+                else { Information += " no an adult. "; }
+                Information += $"In western system your zodiac sign is {person.SunSign} and in Chinese is {person.ChineseSign}. And today is ";
+                if (person.IsBirthday) { Information += "your birhtday."; }
+                else { Information += "not your birhtday."; }
+            }
+        }
+
+    }
+
+}
